5483e570cbe42c8476c64808cbf4c2c7517c01ba
吳松澤 <wusongze@wusongzedeMacBook-Air.local>
2024-11-01 23:14:46 +0800

    finish

--- src/Components/Education.js
+++ src/Components/Education.js
@@ -58,7 +58,7 @@ function Education({ position, company, location, duration, item }) {
             <p className="text-content text-xs md:text-sm font-light pl-1">{location}</p>
           </div>
           <div className="flex items-center">
-            <p className="text-content text-xs md:text-sm font-light pl-1">s
+            <p className="text-content text-xs md:text-sm font-light pl-1">
             </p>
           </div>
         </div>
