2f530de8c7b75155f637eb81f5eaf4a8521f46c5
JayWu <jaywu05120512@gmail.com>
2025-06-26 00:30:17 +0800

    change resume

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -75,7 +75,7 @@ function Header() {
             </NavLink>
           </li>
           <li>
-            <a href="https://drive.google.com/file/d/1YRoXKksix-I7Xc1FLx_-hONoIcjWUaot/view?usp=sharing" target="_blank" rel="noopener noreferrer" onClick={toggleClass}>
+            <a href="https://drive.google.com/file/d/1qhsLJfyyxpMfvso4VnIqYvnTVTzuP63I/view?usp=sharing" target="_blank" rel="noopener noreferrer" onClick={toggleClass}>
               Resume
             </a>
           </li>
